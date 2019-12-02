80b86de961b6b6c89cb94746c36736c6bb938dd8
ElizabethGua <41478817+ElizabethGua@users.noreply.github.com>
2019-12-02 13:25:28 -0500

    Create hello_world.cpp

---
+++ hello_world.cpp
@@ -0,0 +1,13 @@
+//Name: Elizabeth Guayara
+//Email: Elizabeth.guayara70@myhunter.cuny.edu
+//Date: October 22,2019
+//Prints "Hello World!" in C++ language
+
+//include <iostream>        //The built-in library for input/output
+using namespace std;       //The names of standard definitions
+
+int main()                 //C++ programs all have a main() function
+{
+  cout << "Hello, World!"; //Print "Hello, World!" to the terminal
+  return 0;                //Standard way to end function
+}
